78e284214b59ae9646bc2ec9636cf224e222618f
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2020-11-08 18:03:04 +0100

    Added Google Tag Manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,23 @@ module.exports = {
         }],
       },
     },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-WP8KW96",
+  
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
