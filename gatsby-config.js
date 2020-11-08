257815bf447841282850a8b32fc3d3dc167cc02b
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2020-11-08 17:33:07 +0100

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Daniel Tängerfors`,
+    description: `A driven and multifaceted frontend developer with a background in design and photography.`,
+    author: `@dtangerfors`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,10 +21,29 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000`,
+        theme_color: `#f7f7f7`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/juice-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: false
+      }
+    },
+    `gatsby-plugin-transition-link`,
+    {
+      resolve: '@prismicio/gatsby-source-prismic-graphql',
+      options: {
+        repositoryName: 'dtangerforsportfolio',
+        pages: [{ // (optional, builds pages dynamically)
+          type: 'project',         // TypeName from prismic
+          match: '/work/:uid',  // Pages will be generated under this pattern
+          path: '/projects',        // Placeholder page for unpublished documents
+          component: require.resolve('./src/templates/project.js'),
+        }],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
