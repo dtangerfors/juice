db71c91ccefc6b51a9024a2249a9a716d498792e
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2020-11-08 16:57:52 +0100

    Updated design

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,28 @@
 import React from "react"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import Blob from "../components/Blob"
+
+import { PrimaryHeading, Paragraph } from "../components/typography"
+
+
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <TextWrapper>
+      <PrimaryHeading>404 Not found</PrimaryHeading>
+      <Paragraph>You just hit a route that doesn&#39;t exist... the sadness.</Paragraph>
+    </TextWrapper>
+    <Blob/>
   </Layout>
 )
 
