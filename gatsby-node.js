0f9a8238a969026b58531859190c305b35f652d4
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2021-03-17 18:54:48 +0100

    Updated file to handle dynamic pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,30 @@
  */
 
 // You can delete this file if you're not using it
+const path = require('path')
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const projects = await graphql(`
+  {
+    allPrismicProject {
+      nodes {
+        id
+        uid
+        type
+        url
+      }
+    }
+  }
+  
+  `)
+
+  projects.data.allPrismicProject.nodes.forEach((project) => {
+    createPage({
+      path: project.url,
+      component: path.resolve(__dirname, 'src/templates/project.js'),
+      context: { ...project },
+    })
+  })
+}
